8bb49e06d2e6db51a5b4e1551bdbb719dd61da48
Neal Troscinski <ntroscinski@gmail.com>
2017-09-13 15:42:36 -0400

    ASL fully written

--- phase1/asl.c
+++ phase1/asl.c
@@ -7,7 +7,7 @@ HIDDEN semd_t* freeList;
 HIDDEN semd_t* asl;
 
 
-semd_t* insertASL(int* semAdd)
+HIDDEN semd_t* getFreeASL(int* semAdd)
 {
     if (freeList == NULL) return NULL;
     semd_t* ret = freeList;
@@ -16,69 +16,67 @@ semd_t* insertASL(int* semAdd)
     return ret; 
 }
 
+semd_t* find(int* semAdd)
+{
+    semd_t* a = asl;
+    semd_t* b = asl->s_next;
+    while (b->s_semAdd != NULL && b->s_semAdd < semAdd)
+    {
+        a = b;
+        b = b->s_next;
+    }
+    // b's semAdd is eol
+    return a;
+}
+
 // insert p into semd_t list where s_semAdd = semAdd
 int insertBlocked (int *semAdd, pcb_t *p)
 {
-    semd_t* temp_semAdd = asl;
-    semd_t* temp_semD = asl->s_next;
-    while (semAdd != temp_semAdd->s_semAdd)
+    semd_t* prev = find(semAdd);
+    if (prev->s_next->s_semAdd == semAdd)
     {
-        // we found a missing semd_t
-        if (semAdd < temp_semD->s_semAdd)
-        {
-            semd_t* new_semd = insertASL(semAdd); // add new semd_t
-            if (new_semd == NULL) return FALSE;
-            new_semd->s_next = temp_semD;
-            temp_semAdd->s_next = new_semd;
-            insertProcQ(new_semd->s_tp, p);
-            return TRUE;
-        }
-    // advance
-    temp_semAdd = temp_semD->s_next;
-    temp_semD = temp_semD->s_next;
+        insertProcQ(prev->s_next->s_tp,p);
+        return TRUE;
     }
-    // we found the actual thing @ temp_semAdd
-    insertProcQ(temp_semAdd->s_tp, p);
+    semd_t* newSem = getFreeASL(semAdd);
+    if (newSem == NULL) return FALSE;
+    newSem->s_next = prev->s_next;
+    prev->s_next = newSem;
+    insertProcQ(newSem->s_tp,p);
     return TRUE;
 }
 
 // remove the top pcb_t from semd_t where s_semAdd == semAdd
 pcb_t* removeBlocked (int *semAdd)
 {
-    semd_t* p = asl;
-    while (p->s_semAdd != MAXINT && p->s_semAdd != semAdd)
+    semd_t* prev = find(semAdd);
+    if (prev->s_next->s_semAdd == semAdd)
     {
-        p = p->s_next;
+        return removeProcQ(prev->s_next->s_tp);
     }
-    // we're at semAdd or MAXINT now
-    if (p->s_semAdd == MAXINT) return NULL;
-    return removeProcQ(p->s_tp);
+    return NULL;
 }
 
-// remove from middle of list
+// remove from middle of a list
 pcb_t* outBlocked (pcb_t *pcb)
 {
-    semd_t* p = asl;
-    pcb_t* ret;
-    while ((ret = outProcQ(p->s_tp,pcb)) == NULL)
+    semd_t* prev = find(pcb->p_semAdd);
+    if (prev->s_next->s_semAdd == pcb->p_semAdd)
     {
-        if (p->s_next == NULL) 
-        {
-            return NULL;
-        }
-        p = p->s_next;
+        return outProcQ(prev->s_next->s_tp, pcb);
     }
-    return ret;
+    return NULL;
 }
 
 // view top pcb_t of a given semaphore
 pcb_t* headBlocked (int *semAdd)
 {
-    semd_t* p = asl;
-    while (p->s_semAdd != semAdd){
-        p = p->s_next;
+    semd_t* prev = find(semAdd);
+    if (prev->s_next->s_semAdd == semAdd)
+    {
+        return headProcQ(prev->s_next->s_tp);
     }
-    return p->s_tp;
+    return NULL;
 }
 
 int aslEmtpy (semd_t** head) 
